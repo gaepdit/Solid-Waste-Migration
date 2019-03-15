32cc35ab36a1e42cc37c619fa258a17be8fb06e6
Tom Karasch <tom.karasch@dnr.ga.gov>
2019-03-15 18:18:33 -0400

    Building EI XML insert scripts

--- SW Server Scripts/Inserts-to-LEMIR/XML-Data/Staging Table Creates/New EIs/MRF_new.sql
+++ SW Server Scripts/Inserts-to-LEMIR/XML-Data/Staging Table Creates/New EIs/MRF_new.sql
@@ -28,180 +28,180 @@ CREATE TABLE [dbo].[EI_MRF]
              [PermitNumber]                      VARCHAR(100) NULL,
       -- Datafield
              [txtPermitNumber_ID]                VARCHAR(100) NULL, --txtPermitNumber
-             [txtPermitNumber_VAL]               VARCHAR(100) NULL, --123-045(BIO)
+             [txtPermitNumber_VAL]               VARCHAR(MAX) NULL, --123-045(BIO)
              [txtPermitNumber_TAG]               VARCHAR(100) NULL, --Permit Number
              [txtPermitNumber_VIS]               VARCHAR(100) NULL, --true
-             [txtPermitNumber_HIS]               VARCHAR(1000) NULL, --123-045(BIO)|1/23/2019 1:00:17 PM|MHeaphy||
+             [txtPermitNumber_HIS]               VARCHAR(MAX) NULL, --123-045(BIO)|1/23/2019 1:00:17 PM|MHeaphy||
              [txtPermitNumber_DES]               VARCHAR(100) NULL, --Permit Number:
-             [txtPermitNumber_COM]               VARCHAR(200) NULL, --NULL
+             [txtPermitNumber_COM]               VARCHAR(MAX) NULL, --NULL
       -- Datafield
              [ddlEnvInterestStatus_ID]           VARCHAR(100) NULL, --ddlEnvInterestStatus
-             [ddlEnvInterestStatus_VAL]          VARCHAR(100) NULL, --Operating
+             [ddlEnvInterestStatus_VAL]          VARCHAR(MAX) NULL, --Operating
              [ddlEnvInterestStatus_TAG]          VARCHAR(100) NULL, --EI Status
              [ddlEnvInterestStatus_VIS]          VARCHAR(100) NULL, --true
-             [ddlEnvInterestStatus_HIS]          VARCHAR(1000) NULL, --Operating|1/23/2019 1:00:17 PM|MHeaphy||
+             [ddlEnvInterestStatus_HIS]          VARCHAR(MAX) NULL, --Operating|1/23/2019 1:00:17 PM|MHeaphy||
              [ddlEnvInterestStatus_DES]          VARCHAR(100) NULL, --EI Status:
-             [ddlEnvInterestStatus_COM]          VARCHAR(200) NULL, --NULL
+             [ddlEnvInterestStatus_COM]          VARCHAR(MAX) NULL, --NULL
       --Datafield
              [ddlOwnershipType_ID]               VARCHAR(100) NULL, --ddlOwnershipType
-             [ddlOwnershipType_VAL]              VARCHAR(100) NULL, --Private Commercial
+             [ddlOwnershipType_VAL]              VARCHAR(MAX) NULL, --Private Commercial
              [ddlOwnershipType_TAG]              VARCHAR(100) NULL, --Ownership Type
              [ddlOwnershipType_VIS]              VARCHAR(100) NULL, --true
-             [ddlOwnershipType_HIS]              VARCHAR(1000) NULL, --Private Commercial|1/23/2019 1:00:17 PM|MHeaphy||
+             [ddlOwnershipType_HIS]              VARCHAR(MAX) NULL, --Private Commercial|1/23/2019 1:00:17 PM|MHeaphy||
              [ddlOwnershipType_DES]              VARCHAR(100) NULL, --Ownership Type:
-             [ddlOwnershipType_COM]              VARCHAR(200) NULL, --NULL
+             [ddlOwnershipType_COM]              VARCHAR(MAX) NULL, --NULL
         -- Datafield
              [txtComment_ID]                     VARCHAR(100) NULL,
-             [txtComment_VAL]                    VARCHAR(100) NULL,
+             [txtComment_VAL]                    VARCHAR(MAX) NULL,
              [txtComment_TAG]                    VARCHAR(100) NULL,
              [txtComment_VIS]                    VARCHAR(100) NULL,
-             [txtComment_HIS]                    VARCHAR(1000) NULL,
+             [txtComment_HIS]                    VARCHAR(MAX) NULL,
              [txtComment_DES]                    VARCHAR(100) NULL,
-             [txtComment_COM]                    VARCHAR(200) NULL,
+             [txtComment_COM]                    VARCHAR(MAX) NULL,
         --DataField
              [txtPermittedCapacity_ID]           VARCHAR(100) NULL,
-             [txtPermittedCapacity_VAL]          VARCHAR(100) NULL,
+             [txtPermittedCapacity_VAL]          VARCHAR(MAX) NULL,
              [txtPermittedCapacity_TAG]          VARCHAR(100) NULL,
              [txtPermittedCapacity_VIS]          VARCHAR(100) NULL,
-             [txtPermittedCapacity_HIS]          VARCHAR(1000) NULL,
+             [txtPermittedCapacity_HIS]          VARCHAR(MAX) NULL,
              [txtPermittedCapacity_DES]          VARCHAR(100) NULL,
-             [txtPermittedCapacity_COM]          VARCHAR(200) NULL,
+             [txtPermittedCapacity_COM]          VARCHAR(MAX) NULL,
         --DataField
              [txtTotalAcreage_ID]                VARCHAR(100) NULL,
-             [txtTotalAcreage_VAL]               VARCHAR(100) NULL,
+             [txtTotalAcreage_VAL]               VARCHAR(MAX) NULL,
              [txtTotalAcreage_TAG]               VARCHAR(100) NULL,
              [txtTotalAcreage_VIS]               VARCHAR(100) NULL,
-             [txtTotalAcreage_HIS]               VARCHAR(1000) NULL,
+             [txtTotalAcreage_HIS]               VARCHAR(MAX) NULL,
              [txtTotalAcreage_DES]               VARCHAR(100) NULL,
-             [txtTotalAcreage_COM]               VARCHAR(200) NULL,
+             [txtTotalAcreage_COM]               VARCHAR(MAX) NULL,
         --DataField
              [rdoGWInstalled_ID]                 VARCHAR(100) NULL,
-             [rdoGWInstalled_VAL]                VARCHAR(100) NULL,
+             [rdoGWInstalled_VAL]                VARCHAR(MAX) NULL,
              [rdoGWInstalled_TAG]                VARCHAR(100) NULL,
              [rdoGWInstalled_VIS]                VARCHAR(100) NULL,
-             [rdoGWInstalled_HIS]                VARCHAR(1000) NULL,
+             [rdoGWInstalled_HIS]                VARCHAR(MAX) NULL,
              [rdoGWInstalled_DES]                VARCHAR(100) NULL,
-             [rdoGWInstalled_COM]                VARCHAR(200) NULL,
+             [rdoGWInstalled_COM]                VARCHAR(MAX) NULL,
         --DataField
              [ddlAssessmentMonitoring_ID]        VARCHAR(100) NULL,
-             [ddlAssessmentMonitoring_VAL]       VARCHAR(100) NULL,
+             [ddlAssessmentMonitoring_VAL]       VARCHAR(MAX) NULL,
              [ddlAssessmentMonitoring_TAG]       VARCHAR(100) NULL,
              [ddlAssessmentMonitoring_VIS]       VARCHAR(100) NULL,
-             [ddlAssessmentMonitoring_HIS]       VARCHAR(1000) NULL,
+             [ddlAssessmentMonitoring_HIS]       VARCHAR(MAX) NULL,
              [ddlAssessmentMonitoring_DES]       VARCHAR(100) NULL,
-             [ddlAssessmentMonitoring_COM]       VARCHAR(200) NULL,
+             [ddlAssessmentMonitoring_COM]       VARCHAR(MAX) NULL,
         --DataField
              [rdoMethaneInstalled_ID]            VARCHAR(100) NULL,
-             [rdoMethaneInstalled_VAL]           VARCHAR(100) NULL,
+             [rdoMethaneInstalled_VAL]           VARCHAR(MAX) NULL,
              [rdoMethaneInstalled_TAG]           VARCHAR(100) NULL,
              [rdoMethaneInstalled_VIS]           VARCHAR(100) NULL,
-             [rdoMethaneInstalled_HIS]           VARCHAR(1000) NULL,
+             [rdoMethaneInstalled_HIS]           VARCHAR(MAX) NULL,
              [rdoMethaneInstalled_DES]           VARCHAR(100) NULL,
-             [rdoMethaneInstalled_COM]           VARCHAR(200) NULL,
+             [rdoMethaneInstalled_COM]           VARCHAR(MAX) NULL,
          --DataField
              [ddlMethaneMonitoringFrequency_ID]  VARCHAR(100) NULL,
-             [ddlMethaneMonitoringFrequency_VAL] VARCHAR(100) NULL,
+             [ddlMethaneMonitoringFrequency_VAL] VARCHAR(MAX) NULL,
              [ddlMethaneMonitoringFrequency_TAG] VARCHAR(100) NULL,
              [ddlMethaneMonitoringFrequency_VIS] VARCHAR(100) NULL,
-             [ddlMethaneMonitoringFrequency_HIS] VARCHAR(1000) NULL,
+             [ddlMethaneMonitoringFrequency_HIS] VARCHAR(MAX) NULL,
              [ddlMethaneMonitoringFrequency_DES] VARCHAR(100) NULL,
-             [ddlMethaneMonitoringFrequency_COM] VARCHAR(200) NULL,
+             [ddlMethaneMonitoringFrequency_COM] VARCHAR(MAX) NULL,
         --DataField
              [rdoUnderDrainInstalled_ID]         VARCHAR(100) NULL,
-             [rdoUnderDrainInstalled_VAL]        VARCHAR(100) NULL,
+             [rdoUnderDrainInstalled_VAL]        VARCHAR(MAX) NULL,
              [rdoUnderDrainInstalled_TAG]        VARCHAR(100) NULL,
              [rdoUnderDrainInstalled_VIS]        VARCHAR(100) NULL,
-             [rdoUnderDrainInstalled_HIS]        VARCHAR(1000) NULL,
+             [rdoUnderDrainInstalled_HIS]        VARCHAR(MAX) NULL,
              [rdoUnderDrainInstalled_DES]        VARCHAR(100) NULL,
-             [rdoUnderDrainInstalled_COM]        VARCHAR(200) NULL,
+             [rdoUnderDrainInstalled_COM]        VARCHAR(MAX) NULL,
         --DataField
              [rdoLFGasInstalled_ID]              VARCHAR(100) NULL,
-             [rdoLFGasInstalled_VAL]             VARCHAR(100) NULL,
+             [rdoLFGasInstalled_VAL]             VARCHAR(MAX) NULL,
              [rdoLFGasInstalled_TAG]             VARCHAR(100) NULL,
              [rdoLFGasInstalled_VIS]             VARCHAR(100) NULL,
-             [rdoLFGasInstalled_HIS]             VARCHAR(1000) NULL,
+             [rdoLFGasInstalled_HIS]             VARCHAR(MAX) NULL,
              [rdoLFGasInstalled_DES]             VARCHAR(100) NULL,
-             [rdoLFGasInstalled_COM]             VARCHAR(200) NULL,
+             [rdoLFGasInstalled_COM]             VARCHAR(MAX) NULL,
         --DataField
              [rdoWasteEnergyOnsite_ID]           VARCHAR(100) NULL,
-             [rdoWasteEnergyOnsite_VAL]          VARCHAR(100) NULL,
+             [rdoWasteEnergyOnsite_VAL]          VARCHAR(MAX) NULL,
              [rdoWasteEnergyOnsite_TAG]          VARCHAR(100) NULL,
              [rdoWasteEnergyOnsite_VIS]          VARCHAR(100) NULL,
-             [rdoWasteEnergyOnsite_HIS]          VARCHAR(1000) NULL,
+             [rdoWasteEnergyOnsite_HIS]          VARCHAR(MAX) NULL,
              [rdoWasteEnergyOnsite_DES]          VARCHAR(100) NULL,
-             [rdoWasteEnergyOnsite_COM]          VARCHAR(200) NULL,
+             [rdoWasteEnergyOnsite_COM]          VARCHAR(MAX) NULL,
          --DataField
              [txtEnergyProduced_ID]              VARCHAR(100) NULL,
-             [txtEnergyProduced_VAL]             VARCHAR(100) NULL,
+             [txtEnergyProduced_VAL]             VARCHAR(MAX) NULL,
              [txtEnergyProduced_TAG]             VARCHAR(100) NULL,
              [txtEnergyProduced_VIS]             VARCHAR(100) NULL,
-             [txtEnergyProduced_HIS]             VARCHAR(1000) NULL,
+             [txtEnergyProduced_HIS]             VARCHAR(MAX) NULL,
              [txtEnergyProduced_DES]             VARCHAR(100) NULL,
-             [txtEnergyProduced_COM]             VARCHAR(200) NULL,
+             [txtEnergyProduced_COM]             VARCHAR(MAX) NULL,
         --DataField
              [txtPermitIssueDate_ID]             VARCHAR(100) NULL,
-             [txtPermitIssueDate_VAL]            VARCHAR(100) NULL,
+             [txtPermitIssueDate_VAL]            VARCHAR(MAX) NULL,
              [txtPermitIssueDate_TAG]            VARCHAR(100) NULL,
              [txtPermitIssueDate_VIS]            VARCHAR(100) NULL,
-             [txtPermitIssueDate_HIS]            VARCHAR(1000) NULL,
+             [txtPermitIssueDate_HIS]            VARCHAR(MAX) NULL,
              [txtPermitIssueDate_DES]            VARCHAR(100) NULL,
-             [txtPermitIssueDate_COM]            VARCHAR(200) NULL,
+             [txtPermitIssueDate_COM]            VARCHAR(MAX) NULL,
         --DataField
              [txtPermitReviewDueDate_ID]         VARCHAR(100) NULL,
-             [txtPermitReviewDueDate_VAL]        VARCHAR(100) NULL,
+             [txtPermitReviewDueDate_VAL]        VARCHAR(MAX) NULL,
              [txtPermitReviewDueDate_TAG]        VARCHAR(100) NULL,
              [txtPermitReviewDueDate_VIS]        VARCHAR(100) NULL,
-             [txtPermitReviewDueDate_HIS]        VARCHAR(1000) NULL,
+             [txtPermitReviewDueDate_HIS]        VARCHAR(MAX) NULL,
              [txtPermitReviewDueDate_DES]        VARCHAR(100) NULL,
-             [txtPermitReviewDueDate_COM]        VARCHAR(200) NULL,
+             [txtPermitReviewDueDate_COM]        VARCHAR(MAX) NULL,
         --DataField
              [txtWasteTypesAccepted_ID]          VARCHAR(100) NULL,
-             [txtWasteTypesAccepted_VAL]         VARCHAR(100) NULL,
+             [txtWasteTypesAccepted_VAL]         VARCHAR(MAX) NULL,
              [txtWasteTypesAccepted_TAG]         VARCHAR(100) NULL,
              [txtWasteTypesAccepted_VIS]         VARCHAR(100) NULL,
-             [txtWasteTypesAccepted_HIS]         VARCHAR(1000) NULL,
+             [txtWasteTypesAccepted_HIS]         VARCHAR(MAX) NULL,
              [txtWasteTypesAccepted_DES]         VARCHAR(100) NULL,
-             [txtWasteTypesAccepted_COM]         VARCHAR(200) NULL,
+             [txtWasteTypesAccepted_COM]         VARCHAR(MAX) NULL,
         --DataField
              [txtMaterialsNotAccepted_ID]        VARCHAR(100) NULL,
-             [txtMaterialsNotAccepted_VAL]       VARCHAR(100) NULL,
+             [txtMaterialsNotAccepted_VAL]       VARCHAR(MAX) NULL,
              [txtMaterialsNotAccepted_TAG]       VARCHAR(100) NULL,
              [txtMaterialsNotAccepted_VIS]       VARCHAR(100) NULL,
-             [txtMaterialsNotAccepted_HIS]       VARCHAR(1000) NULL,
+             [txtMaterialsNotAccepted_HIS]       VARCHAR(MAX) NULL,
              [txtMaterialsNotAccepted_DES]       VARCHAR(100) NULL,
-             [txtMaterialsNotAccepted_COM]       VARCHAR(200) NULL,
+             [txtMaterialsNotAccepted_COM]       VARCHAR(MAX) NULL,
         --DataField
              [txtMaterialsRecovered_ID]          VARCHAR(100) NULL,
-             [txtMaterialsRecovered_VAL]         VARCHAR(100) NULL,
+             [txtMaterialsRecovered_VAL]         VARCHAR(MAX) NULL,
              [txtMaterialsRecovered_TAG]         VARCHAR(100) NULL,
              [txtMaterialsRecovered_VIS]         VARCHAR(100) NULL,
-             [txtMaterialsRecovered_HIS]         VARCHAR(1000) NULL,
+             [txtMaterialsRecovered_HIS]         VARCHAR(MAX) NULL,
              [txtMaterialsRecovered_DES]         VARCHAR(100) NULL,
-             [txtMaterialsRecovered_COM]         VARCHAR(200) NULL,
+             [txtMaterialsRecovered_COM]         VARCHAR(MAX) NULL,
         --DataField
              [txtStorageCapacity_ID]             VARCHAR(100) NULL,
-             [txtStorageCapacity_VAL]            VARCHAR(100) NULL,
+             [txtStorageCapacity_VAL]            VARCHAR(MAX) NULL,
              [txtStorageCapacity_TAG]            VARCHAR(100) NULL,
              [txtStorageCapacity_VIS]            VARCHAR(100) NULL,
-             [txtStorageCapacity_HIS]            VARCHAR(1000) NULL,
+             [txtStorageCapacity_HIS]            VARCHAR(MAX) NULL,
              [txtStorageCapacity_DES]            VARCHAR(100) NULL,
-             [txtStorageCapacity_COM]            VARCHAR(200) NULL,
+             [txtStorageCapacity_COM]            VARCHAR(MAX) NULL,
         --DataField
              [txtStorageDuration_ID]             VARCHAR(100) NULL,
-             [txtStorageDuration_VAL]            VARCHAR(100) NULL,
+             [txtStorageDuration_VAL]            VARCHAR(MAX) NULL,
              [txtStorageDuration_TAG]            VARCHAR(100) NULL,
              [txtStorageDuration_VIS]            VARCHAR(100) NULL,
-             [txtStorageDuration_HIS]            VARCHAR(1000) NULL,
+             [txtStorageDuration_HIS]            VARCHAR(MAX) NULL,
              [txtStorageDuration_DES]            VARCHAR(100) NULL,
-             [txtStorageDuration_COM]            VARCHAR(200) NULL,
+             [txtStorageDuration_COM]            VARCHAR(MAX) NULL,
         --DataField
              [rdoAcceptPublicWaste_ID]           VARCHAR(100) NULL,
-             [rdoAcceptPublicWaste_VAL]          VARCHAR(100) NULL,
+             [rdoAcceptPublicWaste_VAL]          VARCHAR(MAX) NULL,
              [rdoAcceptPublicWaste_TAG]          VARCHAR(100) NULL,
              [rdoAcceptPublicWaste_VIS]          VARCHAR(100) NULL,
-             [rdoAcceptPublicWaste_HIS]          VARCHAR(1000) NULL,
+             [rdoAcceptPublicWaste_HIS]          VARCHAR(MAX) NULL,
              [rdoAcceptPublicWaste_DES]          VARCHAR(100) NULL,
-             [rdoAcceptPublicWaste_COM]          VARCHAR(200) NULL,
+             [rdoAcceptPublicWaste_COM]          VARCHAR(MAX) NULL,
         --Closure XML
              [grdClosure]                        XML NULL,
       --Construction Detail XML
--- SW Server Scripts/Inserts-to-LEMIR/XML-Data/Staging Table Creates/New EIs/OPBR_new.sql
+++ SW Server Scripts/Inserts-to-LEMIR/XML-Data/Staging Table Creates/New EIs/OPBR_new.sql
@@ -28,108 +28,108 @@ CREATE TABLE [dbo].[EI_OPBR]
              [PermitNumber]                    VARCHAR(100) NULL,
        -- Datafield
              [txtPermitNumber_ID]              VARCHAR(100) NULL, --txtPermitNumber
-             [txtPermitNumber_VAL]             VARCHAR(100) NULL, --123-045(BIO)
+             [txtPermitNumber_VAL]             VARCHAR(MAX) NULL, --123-045(BIO)
              [txtPermitNumber_TAG]             VARCHAR(100) NULL, --Permit Number
              [txtPermitNumber_VIS]             VARCHAR(100) NULL, --true
-             [txtPermitNumber_HIS]             VARCHAR(1000) NULL, --123-045(BIO)|1/23/2019 1:00:17 PM|MHeaphy||
+             [txtPermitNumber_HIS]             VARCHAR(MAX) NULL, --123-045(BIO)|1/23/2019 1:00:17 PM|MHeaphy||
              [txtPermitNumber_DES]             VARCHAR(100) NULL, --Permit Number:
-             [txtPermitNumber_COM]             VARCHAR(200) NULL, --NULL
+             [txtPermitNumber_COM]             VARCHAR(MAX) NULL, --NULL
        -- Datafield
              [ddlEnvInterestStatus_ID]         VARCHAR(100) NULL, --ddlEnvInterestStatus
-             [ddlEnvInterestStatus_VAL]        VARCHAR(100) NULL, --Operating
+             [ddlEnvInterestStatus_VAL]        VARCHAR(MAX) NULL, --Operating
              [ddlEnvInterestStatus_TAG]        VARCHAR(100) NULL, --EI Status
              [ddlEnvInterestStatus_VIS]        VARCHAR(100) NULL, --true
-             [ddlEnvInterestStatus_HIS]        VARCHAR(1000) NULL, --Operating|1/23/2019 1:00:17 PM|MHeaphy||
+             [ddlEnvInterestStatus_HIS]        VARCHAR(MAX) NULL, --Operating|1/23/2019 1:00:17 PM|MHeaphy||
              [ddlEnvInterestStatus_DES]        VARCHAR(100) NULL, --EI Status:
-             [ddlEnvInterestStatus_COM]        VARCHAR(200) NULL, --NULL
+             [ddlEnvInterestStatus_COM]        VARCHAR(MAX) NULL, --NULL
         --Datafield
              [ddlOwnershipType_ID]             VARCHAR(100) NULL, --ddlOwnershipType
-             [ddlOwnershipType_VAL]            VARCHAR(100) NULL, --Private Commercial
+             [ddlOwnershipType_VAL]            VARCHAR(MAX) NULL, --Private Commercial
              [ddlOwnershipType_TAG]            VARCHAR(100) NULL, --Ownership Type
              [ddlOwnershipType_VIS]            VARCHAR(100) NULL, --true
-             [ddlOwnershipType_HIS]            VARCHAR(1000) NULL, --Private Commercial|1/23/2019 1:00:17 PM|MHeaphy||
+             [ddlOwnershipType_HIS]            VARCHAR(MAX) NULL, --Private Commercial|1/23/2019 1:00:17 PM|MHeaphy||
              [ddlOwnershipType_DES]            VARCHAR(100) NULL, --Ownership Type:
-             [ddlOwnershipType_COM]            VARCHAR(200) NULL, --NULL
+             [ddlOwnershipType_COM]            VARCHAR(MAX) NULL, --NULL
         --DataField
              [ddlOtherPBRType_ID]              VARCHAR(100) NULL,
-             [ddlOtherPBRType_VAL]             VARCHAR(100) NULL,
+             [ddlOtherPBRType_VAL]             VARCHAR(MAX) NULL,
              [ddlOtherPBRType_TAG]             VARCHAR(100) NULL,
              [ddlOtherPBRType_VIS]             VARCHAR(100) NULL,
-             [ddlOtherPBRType_HIS]             VARCHAR(1000) NULL,
+             [ddlOtherPBRType_HIS]             VARCHAR(MAX) NULL,
              [ddlOtherPBRType_DES]             VARCHAR(100) NULL,
-             [ddlOtherPBRType_COM]             VARCHAR(200) NULL,
+             [ddlOtherPBRType_COM]             VARCHAR(MAX) NULL,
         --DataField
              [txtOtherPBRType_Other_ID]        VARCHAR(100) NULL,
-             [txtOtherPBRType_Other_VAL]       VARCHAR(100) NULL,
+             [txtOtherPBRType_Other_VAL]       VARCHAR(MAX) NULL,
              [txtOtherPBRType_Other_TAG]       VARCHAR(100) NULL,
              [txtOtherPBRType_Other_VIS]       VARCHAR(100) NULL,
-             [txtOtherPBRType_Other_HIS]       VARCHAR(1000) NULL,
+             [txtOtherPBRType_Other_HIS]       VARCHAR(MAX) NULL,
              [txtOtherPBRType_Other_DES]       VARCHAR(100) NULL,
-             [txtOtherPBRType_Other_COM]       VARCHAR(200) NULL,
+             [txtOtherPBRType_Other_COM]       VARCHAR(MAX) NULL,
         -- Datafield
              [txtComment_ID]                   VARCHAR(100) NULL,
-             [txtComment_VAL]                  VARCHAR(100) NULL,
+             [txtComment_VAL]                  VARCHAR(MAX) NULL,
              [txtComment_TAG]                  VARCHAR(100) NULL,
              [txtComment_VIS]                  VARCHAR(100) NULL,
-             [txtComment_HIS]                  VARCHAR(1000) NULL,
+             [txtComment_HIS]                  VARCHAR(MAX) NULL,
              [txtComment_DES]                  VARCHAR(100) NULL,
-             [txtComment_COM]                  VARCHAR(200) NULL,
+             [txtComment_COM]                  VARCHAR(MAX) NULL,
         --DataField
              [txtFacilityStartDate_ID]         VARCHAR(100) NULL,
-             [txtFacilityStartDate_VAL]        VARCHAR(100) NULL,
+             [txtFacilityStartDate_VAL]        VARCHAR(MAX) NULL,
              [txtFacilityStartDate_TAG]        VARCHAR(100) NULL,
              [txtFacilityStartDate_VIS]        VARCHAR(100) NULL,
-             [txtFacilityStartDate_HIS]        VARCHAR(1000) NULL,
+             [txtFacilityStartDate_HIS]        VARCHAR(MAX) NULL,
              [txtFacilityStartDate_DES]        VARCHAR(100) NULL,
-             [txtFacilityStartDate_COM]        VARCHAR(200) NULL,
+             [txtFacilityStartDate_COM]        VARCHAR(MAX) NULL,
         --DataField
              [txtPBRApprovalDate_ID]           VARCHAR(100) NULL,
-             [txtPBRApprovalDate_VAL]          VARCHAR(100) NULL,
+             [txtPBRApprovalDate_VAL]          VARCHAR(MAX) NULL,
              [txtPBRApprovalDate_TAG]          VARCHAR(100) NULL,
              [txtPBRApprovalDate_VIS]          VARCHAR(100) NULL,
-             [txtPBRApprovalDate_HIS]          VARCHAR(1000) NULL,
+             [txtPBRApprovalDate_HIS]          VARCHAR(MAX) NULL,
              [txtPBRApprovalDate_DES]          VARCHAR(100) NULL,
-             [txtPBRApprovalDate_COM]          VARCHAR(200) NULL,
+             [txtPBRApprovalDate_COM]          VARCHAR(MAX) NULL,
         --DataField
              [txtAcceptingWasteCeasedDate_ID]  VARCHAR(100) NULL,
-             [txtAcceptingWasteCeasedDate_VAL] VARCHAR(100) NULL,
+             [txtAcceptingWasteCeasedDate_VAL] VARCHAR(MAX) NULL,
              [txtAcceptingWasteCeasedDate_TAG] VARCHAR(100) NULL,
              [txtAcceptingWasteCeasedDate_VIS] VARCHAR(100) NULL,
-             [txtAcceptingWasteCeasedDate_HIS] VARCHAR(1000) NULL,
+             [txtAcceptingWasteCeasedDate_HIS] VARCHAR(MAX) NULL,
              [txtAcceptingWasteCeasedDate_DES] VARCHAR(100) NULL,
-             [txtAcceptingWasteCeasedDate_COM] VARCHAR(200) NULL,
+             [txtAcceptingWasteCeasedDate_COM] VARCHAR(MAX) NULL,
         --DataField
              [txtClosureAcknowledgedDate_ID]   VARCHAR(100) NULL,
-             [txtClosureAcknowledgedDate_VAL]  VARCHAR(100) NULL,
+             [txtClosureAcknowledgedDate_VAL]  VARCHAR(MAX) NULL,
              [txtClosureAcknowledgedDate_TAG]  VARCHAR(100) NULL,
              [txtClosureAcknowledgedDate_VIS]  VARCHAR(100) NULL,
-             [txtClosureAcknowledgedDate_HIS]  VARCHAR(1000) NULL,
+             [txtClosureAcknowledgedDate_HIS]  VARCHAR(MAX) NULL,
              [txtClosureAcknowledgedDate_DES]  VARCHAR(100) NULL,
-             [txtClosureAcknowledgedDate_COM]  VARCHAR(200) NULL,
+             [txtClosureAcknowledgedDate_COM]  VARCHAR(MAX) NULL,
         --DataField
              [txtTotalAcreage_ID]              VARCHAR(100) NULL,
-             [txtTotalAcreage_VAL]             VARCHAR(100) NULL,
+             [txtTotalAcreage_VAL]             VARCHAR(MAX) NULL,
              [txtTotalAcreage_TAG]             VARCHAR(100) NULL,
              [txtTotalAcreage_VIS]             VARCHAR(100) NULL,
-             [txtTotalAcreage_HIS]             VARCHAR(1000) NULL,
+             [txtTotalAcreage_HIS]             VARCHAR(MAX) NULL,
              [txtTotalAcreage_DES]             VARCHAR(100) NULL,
-             [txtTotalAcreage_COM]             VARCHAR(200) NULL,
+             [txtTotalAcreage_COM]             VARCHAR(MAX) NULL,
         --DataField
              [txtWasteDescription_ID]          VARCHAR(100) NULL,
-             [txtWasteDescription_VAL]         VARCHAR(100) NULL,
+             [txtWasteDescription_VAL]         VARCHAR(MAX) NULL,
              [txtWasteDescription_TAG]         VARCHAR(100) NULL,
              [txtWasteDescription_VIS]         VARCHAR(100) NULL,
-             [txtWasteDescription_HIS]         VARCHAR(1000) NULL,
+             [txtWasteDescription_HIS]         VARCHAR(MAX) NULL,
              [txtWasteDescription_DES]         VARCHAR(100) NULL,
-             [txtWasteDescription_COM]         VARCHAR(200) NULL,
+             [txtWasteDescription_COM]         VARCHAR(MAX) NULL,
         --DataField
              [rdoAcceptPublicWaste_ID]         VARCHAR(100) NULL,
-             [rdoAcceptPublicWaste_VAL]        VARCHAR(100) NULL,
+             [rdoAcceptPublicWaste_VAL]        VARCHAR(MAX) NULL,
              [rdoAcceptPublicWaste_TAG]        VARCHAR(100) NULL,
              [rdoAcceptPublicWaste_VIS]        VARCHAR(100) NULL,
-             [rdoAcceptPublicWaste_HIS]        VARCHAR(1000) NULL,
+             [rdoAcceptPublicWaste_HIS]        VARCHAR(MAX) NULL,
              [rdoAcceptPublicWaste_DES]        VARCHAR(100) NULL,
-             [rdoAcceptPublicWaste_COM]        VARCHAR(200) NULL,
+             [rdoAcceptPublicWaste_COM]        VARCHAR(MAX) NULL,
          --Internal tracking
              [FACILITY_ID_REF]                 VARCHAR(100) NOT NULL
              )
